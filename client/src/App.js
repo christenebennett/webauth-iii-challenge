d4435590b6414ff494a3db22bc5714e6fa770ce1
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-04-03 21:58:21 -0700

    Set up routes and nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './login/Login';
+import Users from './users/Users';
+import { NavLink, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <nav>
+            <NavLink to="/login">Login</NavLink>
+            &nbsp; | &nbsp;
+            <NavLink to="/users">Users</NavLink>
+            &nbsp; | &nbsp;
+            <button>Logout</button>
+          </nav>
+          <main>
+            <Route path="/login" component={Login}/>
+            <Route path="/users" component={Users}/>
+          </main>
         </header>
       </div>
     );
