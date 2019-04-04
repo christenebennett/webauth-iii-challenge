20602981bd869504646fe6b91eb4a7d9c76b4233
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-04-03 22:00:59 -0700

    Create users component with funtionality to render list of users from api

---
+++ client/src/users/Users.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from 'axios';
+
+class Users extends React.Component {
+
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    const endpoint = "http://localhost:9090/api/users"
+    const headers = { authorization: localStorage.getItem('jwt') }
+    axios
+      .get(endpoint, { headers })
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          users: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  render(){
+    return (
+      <>
+        <h2>Users</h2>
+        <ul>
+          {this.state.users.map(user => (
+            <li>{user.username}</li>
+          ))}
+        </ul>
+      </>
+    )
+  }
+}
+
+export default Users;
